9de9a58f05b81760ab5ecf6a6178530cd4b5c623
jfuller <jfuller@redhat.com>
2022-07-14 06:48:02 +0200

    PSDEVOPS-3750 - generically handle throwing 400 or 404 in REST API

--- config/settings/base.py
+++ config/settings/base.py
@@ -269,6 +269,7 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    "EXCEPTION_HANDLER": "corgi.api.exception_handlers.exception_handler",
 }
 
 

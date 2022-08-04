eb4c5753a90277a8c0443e26594fe51e78b328a8
Justin Spencer <juspence+gitlab@redhat.com>
2022-08-04 13:04:36 +0000

    Try again to fix retry bug

--- corgi/tasks/common.py
+++ corgi/tasks/common.py
@@ -31,7 +31,10 @@ def fatal_code(e):
     """Do not retry on 4xx responses."""
     # Handle requests.exceptions.RequestException
     # 408 is "Request Timeout" that Brew sometimes returns, which can be retried safely
-    if hasattr(e, "response"):
+    # Note http.client.RemoteDisconnected errors have a response attr, but it's set to None / doesn't have a status code
+    # so hasattr doesn't work, and getattr without "is not None" doesn't work either
+    # because response objects are True for 200ish codes, False for 400ish codes
+    if getattr(e, "response", None) is not None:
         return 400 <= e.response.status_code < 500 and e.response.status_code != 408
 
 

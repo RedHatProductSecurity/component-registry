RedHatProductSecurity/component-registry

commit 36c14408598ec397a790fae7db51c211e836ef5e

Rename build to component
Brew.get_component_data returns a component tree, not a build.

corgi/collectors/brew.py
@@ -687,7 +687,8 @@ class Brew:
             if build["cg_name"] != "atomic-reactor":
                 # Container images are built in OSBS, which uses atomic-reactor to build them.
                 raise BrewBuildTypeNotSupported(
-                    f"Image build {build_id} is not supported: {build['cg_name']} content generator used"
+                    f"Image build {build_id} is not supported: "
+                    f"{build['cg_name']} content generator used"
                 )
         build["type"] = build_type
 
@@ -702,6 +703,10 @@ class Brew:
                 # Some older builds do not specify source URLs; the below date was chosen based
                 # on some initial analysis of source-less builds in Brew.
                 if datetime.fromtimestamp(build["completion_ts"]) < datetime(2015, 1, 1):
+                    logger.error(
+                        f"Build {build_id} has no associated source URL but is too old "
+                        f"to process; returning an empty component."
+                    )
                     return {}
                 else:
                     raise exc
@@ -724,7 +729,9 @@ class Brew:
         elif build_type == self.MODULE_BUILD_TYPE:
             component = self.get_module_build_data(build)
         else:
-            return {}
+            raise BrewBuildTypeNotSupported(
+                f"Build {build_id} of type {build_type} is not supported"
+            )
 
         component["build_meta"] = {"build_info": build, "type_info": build_type_info}
         return component

corgi/tasks/brew.py
@@ -29,17 +29,21 @@ def slow_fetch_brew_build(build_id: int, save_product: bool = True, force_proces
     else:
         logger.info("Fetching brew build with build_id: %s", build_id)
 
-    build = Brew().get_component_data(build_id)
-    build_meta = build["build_meta"]["build_info"]
+    component = Brew().get_component_data(build_id)
+    if not component:
+        logger.info("No data fetched for build %s from Brew, exiting...", build_id)
+        return
+
+    build_meta = component["build_meta"]["build_info"]
     build_meta["corgi_ingest_start_dt"] = dateformat.format(timezone.now(), "Y-m-d H:i:s")
     build_meta["corgi_ingest_status"] = "INPROGRESS"
 
     softwarebuild, created = SoftwareBuild.objects.get_or_create(
-        build_id=build["build_meta"]["build_info"]["build_id"],
+        build_id=component["build_meta"]["build_info"]["build_id"],
         defaults={
             "type": SoftwareBuild.Type.BREW,
-            "name": build["meta"]["name"],
-            "source": build["build_meta"]["build_info"]["source"],
+            "name": component["meta"]["name"],
+            "source": component["build_meta"]["build_info"]["source"],
             "meta_attr": build_meta,
         },
     )
@@ -49,16 +53,18 @@ def slow_fetch_brew_build(build_id: int, save_product: bool = True, force_proces
         logger.warning("SoftwareBuild with build_id %s already existed, not reprocessing", build_id)
         return
 
-    if build["type"] == "rpm":
-        root_node = save_srpm(softwarebuild, build)
-    elif build["type"] == "image":
-        root_node = save_container(softwarebuild, build)
-    elif build["type"] == "module":
-        root_node = save_module(softwarebuild, build)
+    if component["type"] == "rpm":
+        root_node = save_srpm(softwarebuild, component)
+    elif component["type"] == "image":
+        root_node = save_container(softwarebuild, component)
+    elif component["type"] == "module":
+        root_node = save_module(softwarebuild, component)
     else:
-        raise BrewBuildTypeNotSupported(f"Build {build_id} type is not supported: {build['type']}")
+        raise BrewBuildTypeNotSupported(
+            f"Build {build_id} type is not supported: {component['type']}"
+        )
 
-    for c in build.get("components", []):
+    for c in component.get("components", []):
         save_component(c, root_node, softwarebuild)
 
     # We don't call save_product_taxonomy by default to allow async call of slow_load_errata task
@@ -77,7 +83,8 @@ def slow_fetch_brew_build(build_id: int, save_product: bool = True, force_proces
             for e in build_meta["errata_tags"]:
                 slow_load_errata.delay(e)
 
-    build_ids = build.get("nested_builds", ())
+    build_ids = component.get("nested_builds", ())
+    logger.info("Fetching brew builds for %s", build_ids)
     for b_id in build_ids:
         logger.info("Requesting fetch of nested build: %s", b_id)
         slow_fetch_brew_build.delay(b_id)

commit 0215bd3c1fbff57783bb3d8c57a6410f1dd46613

Account for unknown build types
This is a regression from #113. If we encounter a build type that isn't
supported, the next() call ends with StopIteration and an exception is
emitted. Add a default that isn't included in the supported build type list
so we raise the correct exception, `BrewBuildTypeNotSupported`.

corgi/collectors/brew.py
@@ -679,7 +679,8 @@ class Brew:
         # Determine build type
         build_type_info = self.koji_session.getBuildType(build)
         build_type = next(
-            type_ for type_ in build_type_info.keys() if type_ in self.SUPPORTED_BUILD_TYPES
+            (type_ for type_ in build_type_info.keys() if type_ in self.SUPPORTED_BUILD_TYPES),
+            "unknown",
         )
         if not any(type_ in self.SUPPORTED_BUILD_TYPES for type_ in build_type_info.keys()):
             raise BrewBuildTypeNotSupported(
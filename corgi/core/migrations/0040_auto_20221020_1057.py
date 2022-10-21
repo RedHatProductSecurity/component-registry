b40d9125489159661052ef212efeb6e1f07f45e8
jfuller <jfuller@redhat.com>
2022-10-21 13:12:35 +0200

    remove additional migration

--- corgi/core/migrations/0040_auto_20221020_1057.py
+++ corgi/core/migrations/0040_auto_20221020_1057.py
@@ -26,4 +26,22 @@ class Migration(migrations.Migration):
             model_name="productversion",
             index=models.Index(fields=["ofuri"], name="core_produc_ofuri_c62cf2_idx"),
         ),
+        migrations.RunSQL(
+            "CREATE INDEX core_compon_latest_name_type_idx ON public.core_component \
+             USING btree (name, type) WHERE (((type)::text = 'SRPM'::text) OR \
+             (((arch)::text = 'noarch'::text) AND ((type)::text = 'CONTAINER_IMAGE'::text)) \
+             OR ((type)::text = 'RHEL_MODULE'::text))"
+        ),
+        migrations.RunSQL(
+            "CREATE INDEX core_compon_latest_type_name_idx ON public.core_component \
+             USING btree (type, name) WHERE (((type)::text = 'SRPM'::text) OR \
+             (((arch)::text = 'noarch'::text) AND ((type)::text = 'CONTAINER_IMAGE'::text)) \
+             OR ((type)::text = 'RHEL_MODULE'::text))"
+        ),
+        migrations.RunSQL(
+            "CREATE INDEX core_compon_latest_idx ON public.core_component \
+            USING btree (uuid, software_build_id, type, name, product_streams) \
+            WHERE (((type)::text = 'SRPM'::text) OR (((arch)::text = 'noarch'::text) \
+            AND ((type)::text = 'CONTAINER_IMAGE'::text)) OR ((type)::text = 'RHEL_MODULE'::text))"
+        ),
     ]

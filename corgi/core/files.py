5848b4ff86cacbe616da2612f1e34a4f21cbe847
Justin Spencer <juspence+gpg@redhat.com>
2022-10-24 19:30:58 +0000

    Make generic ManifestFile abstract class for both products and components

--- corgi/core/files.py
+++ corgi/core/files.py
@@ -1,30 +1,34 @@
 import json
 import logging
+from abc import ABC, abstractmethod
 
 from django.template.loader import render_to_string
+
+from corgi.core.mixins import TimeStampedModel
 
 logger = logging.getLogger(__name__)
 
 
-class ProductManifestFile:
-    """A data file that represents a product manifest in machine-readable SPDX / JSON format."""
+class ManifestFile(ABC):
+    """A data file that represents a generic manifest in machine-readable SPDX / JSON format."""
 
-    file_name = "product_manifest.json"  # Name of the Django template, not the final file itself.
-    # We use the same template file for all products we want to manifest
-    # We can subclass this to handle different Product subclasses with different properties
-    # Or to handle different ways of generating manifest properties from Product properties
+    @property
+    @abstractmethod
+    def file_name(self) -> str:
+        """Name of the Django template, not the final file itself."""
+        pass
 
-    def __init__(self, product) -> None:
-        self.product = product  # Product (or subclass) model instance
+    def __init__(self, obj: TimeStampedModel) -> None:
+        self.obj = obj  # Model instance to manifest (either Component or Product)
 
     def render_content(self) -> str:
-        kwargs_for_template = {"product": self.product}
+        kwargs_for_template = {"obj": self.obj}
         content = render_to_string(self.file_name, kwargs_for_template)
 
         return self._validate_and_clean(content)
 
     @staticmethod
-    def _validate_and_clean(content) -> str:
+    def _validate_and_clean(content: str) -> str:
         """Raise an exception if content for SPDX file is not valid JSON"""
         # The manifest template must use Django's escapejs filter,
         # to generate valid JSON and escape quotes + newlines
@@ -32,3 +36,19 @@ class ProductManifestFile:
         # so we convert from JSON back to JSON to get "\n" instead
         content = json.loads(content)
         return json.dumps(content, indent=2, sort_keys=True)
+
+
+class ComponentManifestFile(ManifestFile):
+    """A data file that represents a component manifest in machine-readable SPDX / JSON format."""
+
+    file_name = "component_manifest.json"  # Name of the Django template, not the final file itself.
+    # We use the same template file for all components we want to manifest
+
+
+class ProductManifestFile(ManifestFile):
+    """A data file that represents a product manifest in machine-readable SPDX / JSON format."""
+
+    file_name = "product_manifest.json"  # Name of the Django template, not the final file itself.
+    # We use the same template file for all products we want to manifest
+    # We can subclass this to handle different Product subclasses with different properties
+    # Or to handle different ways of generating manifest properties from Product properties

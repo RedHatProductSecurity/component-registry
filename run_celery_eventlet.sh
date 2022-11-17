a5ea19d79b0d3c8b78c1453bd32261fd2faac7aa
Justin Spencer <juspence+gpg@redhat.com>
2022-11-17 13:38:44 +0000

    Add eventlet Celery script and workers for I/O bound tasks
    The fast worker pulls tasks only from the fast queue, and should
    never get stuck doing "slow" CPU-bound or I/O-bound work.
    
    The eventlet worker pulls tasks from the eventlet queue, and
    should never get stuck doing any CPU-bound work that will block
    a greenthread. These are asynchronous and should only receive
    I/O-bound tasks.
    
    The slow worker pulls tasks from the fast, eventlet, and slow
    queues. This is the only worker that processes tasks in the slow
    queue, so tasks there must not be time-sensitive. They may be delayed
    significantly and are not guaranteed to be picked up right away.

--- run_celery_eventlet.sh
+++ run_celery_eventlet.sh
@@ -3,7 +3,7 @@
 # custom run script for starting corgi celery service in corgi-stage and corgi-prod environments.
 
 # Remove any left-over PID files in case the container is being restarted to prevent errors such as:
-# ERROR: Pidfile (/tmp/slow.pid) already exists.
-rm -f /tmp/slow.pid
+# ERROR: Pidfile (/tmp/eventlet.pid) already exists.
+rm -f /tmp/eventlet.pid
 
-exec celery -A config worker -E --loglevel info --pidfile /tmp/slow.pid -Q eventlet,slow,fast -n celery@%h
+exec celery -A config worker -E --loglevel info --pidfile /tmp/eventlet.pid -Q eventlet -P eventlet -c 16 -n celery@%h

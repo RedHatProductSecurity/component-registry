0ca1bf435b30a3090cffeeeaa77f699006ee14d3
Martin Prpič <mprpic@redhat.com>
2023-04-17 15:17:31 -0400

    Ensure we spit our errors when cloning inaccessible repos

--- corgi/collectors/syft.py
+++ corgi/collectors/syft.py
@@ -1,5 +1,6 @@
 import json
 import logging
+import os
 import subprocess  # nosec B404
 from pathlib import Path
 from tempfile import TemporaryDirectory
@@ -93,9 +94,20 @@ class Syft:
         """
         with TemporaryDirectory(dir=settings.SCA_SCRATCH_DIR) as scan_dir:
             logger.info("Cloning %s to %s", target_url, scan_dir)
-            subprocess.check_call(
-                ["/usr/bin/git", "clone", target_url, scan_dir], stderr=subprocess.DEVNULL
+            # This may fail if we don't have access to the repository. GIT_TERMINAL_PROMPT=0
+            # ensures that we don't hang the command on a prompt for a username and password.
+            env = dict(os.environ, GIT_TERMINAL_PROMPT="0")
+            result = subprocess.run(
+                ["/usr/bin/git", "clone", target_url, scan_dir],
+                capture_output=True,
+                timeout=120,  # seconds
+                env=env,
             )  # nosec B603
+            if result.returncode != 0:
+                raise ValueError(
+                    f"git clone of {target_url} failed with: {result.stderr.decode('utf-8')}"
+                )
+
             if target_ref:
                 subprocess.check_call(  # nosec B603
                     ["/usr/bin/git", "checkout", target_ref],

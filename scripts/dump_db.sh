09144bfc1e732cfa6e7b1bc77f7b2e14af40795d
Jason Shepherd <jason@jasonshepherd.net>
2023-06-26 11:02:52 +1000

    allow dumpt_db script to work in a container by skipping podman running check, psql command will fail anyway

--- scripts/dump_db.sh
+++ scripts/dump_db.sh
@@ -3,9 +3,8 @@ set -e
 
 export PGPASSWORD=test
 
-if ! podman ps | grep 'corgi-db' &>/dev/null; then
-    echo 'error: corgi "corgi-db" container does not appear to be running.'
-    exit 1
-fi
+# Use 2 parallel threads to dump the database in directory format
+# Using directory format allows us to potentially restore restore to AWS RDS
+# Having multiple files allows rsync to work more effectively.
 
-pg_dump -v -j 2 -F d -f corgi.db -U corgi-db-user -h localhost -p 5433 corgi-db
+pg_dump --verboase --jobs=2 --format=d --file=corgi.db --username=corgi-db-user --host=localhost --port=5433 corgi-db

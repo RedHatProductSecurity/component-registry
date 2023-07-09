bea95c1aa119a9f0617e937354dd73cd84919ab5
Jason Shepherd <jason@jasonshepherd.net>
2023-07-10 06:50:49 +1000

    look for modules in errata build list and persist collector modules ready for processing by slow_fetch_modular_builds

--- corgi/collectors/errata_tool.py
+++ corgi/collectors/errata_tool.py
@@ -7,11 +7,13 @@ import requests
 from django.conf import settings
 from requests_gssapi import HTTPSPNEGOAuth
 
+from corgi.collectors.brew import Brew
 from corgi.collectors.models import (
     CollectorErrataProduct,
     CollectorErrataProductVariant,
     CollectorErrataProductVersion,
 )
+from corgi.core.models import SoftwareBuild
 
 logger = logging.getLogger(__name__)
 
@@ -149,11 +151,17 @@ class ErrataTool:
           }
         }
         """
-        variant_to_component_map = defaultdict(list)
+        variant_to_component_map: typing.DefaultDict[str, list] = defaultdict(list)
         builds = self.get(f"api/v1/erratum/{erratum_id}/builds_list.json")
+        brew = Brew(SoftwareBuild.Type.BREW)
         for product_version in builds.values():
             for build in product_version["builds"]:
                 for build_nvr, build_data in build.items():
+                    if build_data["is_module"]:
+                        self._parse_module(
+                            build_nvr, build_data["variant_arch"], brew, variant_to_component_map
+                        )
+                        continue
                     build_id = build_data["id"]
                     for variant, components_by_arch in build_data["variant_arch"].items():
                         variant_components = []
@@ -225,3 +233,31 @@ class ErrataTool:
             for component in variant:
                 build_ids.update(component.keys())
         return list(build_ids)
+
+    def _parse_module(
+        self,
+        module_name: str,
+        module_data: dict[str, dict[str, list[str]]],
+        brew: Brew,
+        variant_to_component_map: typing.DefaultDict[str, list],
+    ) -> None:
+        """Persist collector models for modular builds from errata build_list.json. This allows
+        later processing of modular builds in the Brew task slow_fetch_modular_build"""
+        for variant, components_by_arch in module_data.items():
+            modular_rpms = []
+            for arch, components in components_by_arch.items():
+                # SRPMS are looked up in persist_modules from Brew via RPMs
+                if arch == "SRPMS":
+                    continue
+                # persist_modules expects the .rpm suffix stripped
+                rpm_suffix_len = ".rpm"
+                stripped_components = [
+                    rpm[: -len(rpm_suffix_len)] for rpm in components if rpm.endswith(".rpm")
+                ]
+                modular_rpms.extend(stripped_components)
+            build_ids = brew.persist_modules({module_name: modular_rpms})
+            for build_id in build_ids:
+                # persist_modules returns a flat list of build_ids, so its no longer possible to
+                # map modular_rpms to build_ids. In slow_save_errata these are only saved as
+                # meta_attr so, let's just save the entire list for each build_id
+                variant_to_component_map[variant].append({build_id: modular_rpms})

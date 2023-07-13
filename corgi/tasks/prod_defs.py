31ea2bb65040f2ac66a1c8493984f9452d259b43
Jason Shepherd <jason@jasonshepherd.net>
2023-07-14 05:57:36 +1000

    revert buggy code for moving variant between streams

--- corgi/tasks/prod_defs.py
+++ corgi/tasks/prod_defs.py
@@ -1,5 +1,5 @@
 import re
-from typing import Any, Optional
+from typing import Any
 
 from celery.utils.log import get_task_logger
 from celery_singleton import Singleton
@@ -12,13 +12,11 @@ from corgi.collectors.models import (
 )
 from corgi.collectors.prod_defs import ProdDefs
 from corgi.core.models import (
-    Component,
     Product,
     ProductNode,
     ProductStream,
     ProductVariant,
     ProductVersion,
-    SoftwareBuild,
 )
 from corgi.tasks.common import RETRY_KWARGS, RETRYABLE_ERRORS
 
@@ -26,66 +24,6 @@ logger = get_task_logger(__name__)
 # Find a substring that looks like a version (e.g. "3", "3.5", "3-5", "1.2.z") at the end of a
 # searched string.
 RE_VERSION_LIKE_STRING = re.compile(r"\d[\dz.-]*$|$")
-
-
-@app.task(base=Singleton, autoretry_for=RETRYABLE_ERRORS, retry_kwargs=RETRY_KWARGS)
-def slow_update_builds_for_variant(
-    variant_name: str,
-    updated_stream: tuple[str, str],
-    updated_version: Optional[tuple[str, str]] = None,
-    updated_product: Optional[tuple[str, str]] = None,
-):
-    """Ensures new stream parent of the passed in variant is reflected in all the variant's builds
-    and child components"""
-    logger.info(
-        f"Updating components related to {variant_name} with new stream details: {updated_stream}"
-    )
-    updated_stream_objs = (
-        ProductStream.objects.get(name=updated_stream[0]),
-        ProductStream.objects.get(name=updated_stream[1]),
-    )
-    updated_version_objs = None
-    updated_product_objs = None
-    if updated_version:
-        logger.info(f"Also updating product_version with {updated_version}")
-        updated_version_objs = (
-            ProductVersion.objects.get(name=updated_version[0]),
-            ProductVersion.objects.get(name=updated_version[1]),
-        )
-    if updated_product:
-        logger.info(f"Also updating product {updated_product}")
-        updated_product_objs = (
-            Product.objects.get(name=updated_product[0]),
-            Product.objects.get(name=updated_product[1]),
-        )
-    for software_build in (
-        SoftwareBuild.objects.filter(relations__product_ref=variant_name).distinct().iterator()
-    ):
-        component = software_build.components.get()
-        _update_product_streams(
-            component, updated_stream_objs, updated_version_objs, updated_product_objs
-        )
-        for cnode in component.cnodes.get_queryset().iterator():
-            for d in cnode.get_descendants().iterator():
-                _update_product_streams(
-                    d.obj, updated_stream_objs, updated_version_objs, updated_product_objs
-                )
-
-
-def _update_product_streams(
-    component: Component,
-    updated_stream: tuple[ProductStream, ProductStream],
-    updated_version: Optional[tuple[ProductVersion, ProductVersion]] = None,
-    updated_product: Optional[tuple[Product, Product]] = None,
-):
-    component.productstreams.add(updated_stream[0])
-    component.productstreams.remove(updated_stream[1])
-    if updated_version:
-        component.productversions.add(updated_version[0])
-        component.productversions.remove(updated_version[1])
-    if updated_product:
-        component.products.add(updated_product[0])
-        component.products.remove(updated_product[1])
 
 
 @app.task(base=Singleton, autoretry_for=RETRYABLE_ERRORS, retry_kwargs=RETRY_KWARGS)
@@ -268,84 +206,29 @@ def parse_variants_from_brew_tags(
                     variant_product_version: CollectorErrataProductVersion = (
                         et_variant.product_version
                     )
-                    # We don't use update_or_create here in order to get the existing product detail
-                    # of the variant. We then clear the existing product details from all associated
-                    # builds and there components before adding the new product details.
-                    variant_created = False
-                    try:
-                        product_variant = ProductVariant.objects.get(name=et_variant.name)
-                        product_variant.cpe = et_variant.cpe
-                        product_variant.meta_attr = {
-                            "et_product": variant_product_version.product.name,
-                            "et_product_version": variant_product_version.name,
-                        }
-                        product_variant.save()
-                    except ProductVariant.DoesNotExist:
-                        product_variant = ProductVariant.objects.create(
-                            name=et_variant.name,
-                            cpe=et_variant.cpe,
-                            products=product,
-                            productversions=product_version,
-                            productstreams=product_stream,
-                            meta_attr={
+                    product_variant, _ = ProductVariant.objects.update_or_create(
+                        name=et_variant.name,
+                        defaults={
+                            "version": "",
+                            "cpe": et_variant.cpe,
+                            "description": "",
+                            "products": product,
+                            "productversions": product_version,
+                            "productstreams": product_stream,
+                            "meta_attr": {
                                 "et_product": variant_product_version.product.name,
                                 "et_product_version": variant_product_version.name,
                             },
-                        )
-                        variant_created = True
-
-                    _, node_created = ProductNode.objects.update_or_create(
+                        },
+                    )
+                    ProductNode.objects.get_or_create(
                         object_id=product_variant.pk,
                         defaults={
                             "parent": product_stream_node,
                             "obj": product_variant,
                         },
                     )
-                    if node_created and variant_created:
-                        # This is a new Variant, so no need to update anything
-                        continue
-
-                    # If there was an existing product variant, and it was updated
-                    # to now be associated with a different stream we need to
-                    # update all the builds to reflect the new product relationships
-                    # Capture all the old stream details before updating the foreign keys
-                    existing_product_name = product_variant.products.name
-                    existing_version_name = product_variant.productversions.name
-                    existing_stream_name = product_variant.productstreams.name
-                    product_variant.products = product
-                    product_variant.productversions = product_version
-                    product_variant.productstreams = product_stream
-                    product_variant.save()
-
-                    changed_products = None
-                    changed_versions = None
-                    if existing_product_name != product.name:
-                        changed_products = (
-                            product.name,
-                            existing_product_name,
-                        )
-                    if existing_version_name != product_version.name:
-                        changed_versions = (
-                            product_version.name,
-                            existing_version_name,
-                        )
-                    slow_update_builds_for_variant.apply_async(
-                        args=(
-                            # Variant name is always present
-                            product_variant.name,
-                            # New and old stream names are always present
-                            (
-                                product_stream.name,
-                                existing_stream_name,
-                            ),
-                            # New and old version names might not be present
-                            changed_versions,
-                            # New and old product names might not be present
-                            changed_products,
-                        ),
-                        # kwarg for Celery, not the task
-                        countdown=300,
-                    )
+                    product_variant.save_product_taxonomy()
 
 
 def parse_errata_info(

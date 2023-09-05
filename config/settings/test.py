82fa3ac2ffa026aa0df471296302e80e64e9409c
jfuller <jfuller@redhat.com>
2023-09-05 07:48:37 +0200

    exp CONN_MAX_AGE

--- config/settings/test.py
+++ config/settings/test.py
@@ -11,8 +11,8 @@ SECRET_KEY = get_random_secret_key()
 
 # Use PG's default admin user to allow creating the test database
 _user = os.getenv("CORGI_DB_USER", "postgres")
-DATABASES["default"]["USER"] = _user  # noqa: F405
-DATABASES["read_only"]["USER"] = _user  # noqa: F405
+DATABASES["default"]["USER"] = _user  # type: ignore # noqa: F405
+DATABASES["read_only"]["USER"] = _user # type: ignore  # noqa: F405
 
 SESSION_COOKIE_SECURE = False
 

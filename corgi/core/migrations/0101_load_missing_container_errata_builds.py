d7ce9850029d2d92a4215ea95db4537c2e791a65
Justin Spencer <juspence+gpg@redhat.com>
2023-12-07 23:08:28 +0000

    Fix migration requiring network access / loading products in tests
    This allows running tox -e corgi-migrations locally, when not connected to the VPN. It also reduces the time the test takes from 43-ish seconds to only 6-ish seconds, when on the VPN.

--- corgi/core/migrations/0101_load_missing_container_errata_builds.py
+++ corgi/core/migrations/0101_load_missing_container_errata_builds.py
@@ -3,15 +3,11 @@ from django.db import migrations
 from corgi.tasks.errata_tool import (
     slow_load_stream_errata as current_load_stream_errata,
 )
-from corgi.tasks.prod_defs import update_products
 from corgi.tasks.tagging import NO_MANIFEST_TAG
 
 
 def load_missing_container_errata_build(apps, schema_editor):
     ProductStream = apps.get_model("core", "ProductStream")
-
-    # pre-load products to make sure variants_from_brew_tags and releases_from_brew_tags are updated
-    update_products()
 
     for stream_name in (
         ProductStream.objects.exclude(components__isnull=True)
